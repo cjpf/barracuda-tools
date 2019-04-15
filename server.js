952b8374d745098ffc33052aa76055d8d3e66e39
CJ <cjpf@charliejuliet.net>
2019-04-15 14:38:29 -0400

    created app get and post for testing dns module

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dns = require('dns');
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({etended: true}));
+const dns = require("dns");
 const port = 5000;
 
-app.get('/api/dnsA', (req, res) => {
-  dns.resolve('charliejuliet.net', 'A', (err, records) => {
-    res.json(records);
-  });
+// Proxy config
+app.set("trust proxy", "loopback");
+
+// Static routes
+// app.use(express.static('public'));
+
+// Express routers
+// app.use('/', require('./dkim.js'));
+
+app.get("/api/dkim", (req, res) => {
+  dns.resolve(
+    "protonmail._domainkey.charliejuliet.net",
+    "TXT",
+    (err, records) => {
+      if (err) {
+        console.log(err);
+      }
+      res.json(records);
+    }
+  );
+});
+
+app.post("/api/dkim", (req, res) => {
+  dns.resolve(
+    "protonmail._domainkey.charliejuliet.net",
+    "TXT",
+    (err, records) => {
+      if (err) {
+        console.log(err);
+      }
+      res.json(records);
+    }
+  );
 });
 
 app.listen(port, () => {
