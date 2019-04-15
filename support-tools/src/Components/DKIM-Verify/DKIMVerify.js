81cf8e4a290aa03b720f357cebdc044eba6d6007
CJ <cjpf@charliejuliet.net>
2019-04-15 14:38:07 -0400

    formatted component source

--- support-tools/src/Components/DKIM-Verify/DKIMVerify.js
+++ support-tools/src/Components/DKIM-Verify/DKIMVerify.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class DKIMVerify extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {};
-    }
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>{"DKIM Verification Tool"}</h1>
-                <input type="file" id="emlpicker" accept=".eml" />
-                <input type="button" value="Verify"/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>{"DKIM Verification Tool"}</h1>
+        <form id="dkim-form" action="#" method="post">
+          <input type="file" id="emlfile" accept=".eml" />
+          <button type="submit" id="dkim-verify-button">
+            Verify
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
