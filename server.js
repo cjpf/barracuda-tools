7a3e37584666cbed171159b2abc82e09336772ec
cj pfenninger <cjpf@charliejuliet.net>
2019-04-28 18:20:57 -0400

    Created cors middleware, and parametrized API route for DKIM lookups
    and created setupProxy.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({etended: true}));
 const dns = require("dns");
+const cors = require("cors");
 const port = 5000;
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors({origin: true}));
 // Proxy config
 app.set("trust proxy", "loopback");
 
@@ -14,30 +15,40 @@ app.set("trust proxy", "loopback");
 // Express routers
 // app.use('/', require('./dkim.js'));
 
-app.get("/api/dkim", (req, res) => {
-  dns.resolve(
-    "protonmail._domainkey.charliejuliet.net",
-    "TXT",
+// app.get("/api/dkim_test", (req, res) => {
+//   dns.resolveTxt(
+//     "protonmail._domainkey.charliejuliet.net",
+//     (err, records) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//       console.log("Retrieved " + records);
+//       res.json(records);
+//     }
+//   );
+// });
+
+app.get("/api/dkim/:hostname", (req, res) => {
+  dns.resolveTxt(
+    req.params.hostname,
     (err, records) => {
       if (err) {
         console.log(err);
       }
+      console.log("Retrieved " + records);
       res.json(records);
     }
   );
 });
 
-app.post("/api/dkim", (req, res) => {
-  dns.resolve(
-    "protonmail._domainkey.charliejuliet.net",
-    "TXT",
-    (err, records) => {
-      if (err) {
-        console.log(err);
-      }
-      res.json(records);
+app.get("/api/a/:domain", (req, res) => {
+  dns.resolve(req.params.domain, "A", (err, records) => {
+    if (err) {
+      console.log(err);
     }
-  );
+    console.log(res.json(records));
+    res.json(records);
+  });
 });
 
 app.listen(port, () => {
