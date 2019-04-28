d734a3170404001f3af8a84334b75d47ccf5f05a
cj pfenninger <cjpf@charliejuliet.net>
2019-04-28 19:24:07 -0400

    Removed test route
    Formatted with ESLint

--- server.js
+++ server.js
@@ -3,46 +3,24 @@ const app = express();
 const dns = require("dns");
 const cors = require("cors");
 const port = 5000;
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors({origin: true}));
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: true }));
 // Proxy config
 app.set("trust proxy", "loopback");
 
-// Static routes
-// app.use(express.static('public'));
-
-// Express routers
-// app.use('/', require('./dkim.js'));
-
-// app.get("/api/dkim_test", (req, res) => {
-//   dns.resolveTxt(
-//     "protonmail._domainkey.charliejuliet.net",
-//     (err, records) => {
-//       if (err) {
-//         console.log(err);
-//       }
-//       console.log("Retrieved " + records);
-//       res.json(records);
-//     }
-//   );
-// });
-
 app.get("/api/dkim/:hostname", (req, res) => {
-  dns.resolveTxt(
-    req.params.hostname,
-    (err, records) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log("Retrieved " + records);
-      res.json(records);
+  dns.resolveTxt(req.params.hostname, (err, records) => {
+    if (err) {
+      console.log(err);
     }
-  );
+    console.log("Retrieved " + records);
+    res.json(records);
+  });
 });
 
-app.get("/api/a/:domain", (req, res) => {
-  dns.resolve(req.params.domain, "A", (err, records) => {
+app.get("/api/a/:hostname", (req, res) => {
+  dns.resolve(req.params.hostname, "A", (err, records) => {
     if (err) {
       console.log(err);
     }
